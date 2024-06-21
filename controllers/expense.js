f2869a9d766fe3dc54bae893535ca3c2451a0981
ankitsaini-github <ankit121saini@gmail.com>
2024-06-21 14:28:11 +0530

    used transactions -improved optimisation

--- controllers/expense.js
+++ controllers/expense.js
@@ -1,5 +1,6 @@
-const Expenses = require('../models/expenses');
-const Users = require('../models/users');
+const Expenses = require("../models/expenses");
+const Users = require("../models/users");
+const sequelize = require("../util/database");
 
 //fetch all expense
 exports.fetchAll = async (req, res) => {
@@ -7,12 +8,12 @@ exports.fetchAll = async (req, res) => {
   try {
     const expenses = await Expenses.findAll({ where: { userId: uid } });
     if (!expenses) {
-      return res.status(404).json({ error: 'No expenses found.' });
+      return res.status(404).json({ error: "No expenses found." });
     }
     res.status(200).json(expenses);
   } catch (err) {
     console.error(err);
-    res.status(500).json({ error: 'Failed to fetch expenses.' });
+    res.status(500).json({ error: "Failed to fetch expenses." });
   }
 };
 
@@ -20,22 +21,30 @@ exports.fetchAll = async (req, res) => {
 exports.addExpense = async (req, res) => {
   const { amount, description, category } = req.body;
   const userId = req.user.id;
-  
+
   if (!amount || !description || !category) {
-    return res.status(400).json({ error: 'All fields are required.' });
+    return res.status(400).json({ error: "All fields are required." });
   }
 
   try {
-    const result = await Expenses.create({ amount, description, category, userId });
+    const result = await sequelize.transaction(async (t) => {
+      const expense = await Expenses.create(
+        { amount, description, category, userId },
+        { transaction: t }
+      );
 
-    const user = await Users.findByPk(userId)
-    user.totalExpense += parseFloat(amount)
-    user.save()
+      const user = await Users.findByPk(userId, { transaction: t });
+
+      user.totalExpense += parseFloat(amount);
+      await user.save({ transaction: t });
+
+      return expense;
+    });
 
     res.status(201).send(result);
   } catch (err) {
-      console.error(err);
-      res.status(500).json({ error: 'Failed to add expense.' });
+    console.error(err);
+    res.status(500).json({ error: "Failed to add expense." });
   }
 };
 
@@ -44,23 +53,35 @@ exports.deleteExpense = async (req, res) => {
   const { id } = req.params;
   const userId = req.user.id;
   try {
-    const expense = await Expenses.findByPk(id);
+    const result = await sequelize.transaction(async (t) => {
+      const expense = await Expenses.findByPk(id, { transaction: t });
 
-    if (!expense) {
-      return res.status(404).json({ error: 'Expense not found.' });
-    }
-    if(expense.userId != userId){
-      return res.status(403).json({ error: 'Unauthorized action.' });
-    }
-    
-    const user = await Users.findByPk(userId)
-    user.totalExpense -= parseFloat(expense.amount)
-    user.save()
-    
-    await expense.destroy();
-    res.status(200).json({ message: 'Expense deleted successfully.' });
+      if (!expense) {
+        return res.status(404).json({ error: "Expense not found." });
+      }
+
+      if (expense.userId != userId) {
+        return res.status(403).json({ error: "Unauthorized action." });
+      }
+
+      await expense.destroy({ transaction: t });
+
+      const user = await Users.findByPk(userId, { transaction: t });
+
+      user.totalExpense -= parseFloat(expense.amount);
+      await user.save({ transaction: t });
+
+      return expense;
+    });
+
+    res
+      .status(200)
+      .json({
+        message: "Expense deleted successfully.",
+        deletedExpense: result,
+      });
   } catch (err) {
     console.error(err);
-    res.status(500).json({ error: 'Failed to delete expense.' });
+    res.status(500).json({ error: "Failed to delete expense." });
   }
 };
